30319ccb41abb6c147bd1c707e84cdcbdb42cedc
Theo Mathieu <tmathieu.github@fastmail.com>
2025-06-15 07:17:14 +0200

    feat: wip

--- src/save/index.ts
+++ src/save/index.ts
@@ -2,28 +2,29 @@ import * as core from '@actions/core'
 import {exec} from '../utils/cache'
 
 async function run(): Promise<void> {
-  try {
-    core.warning('Starting cache save process...')
-    const cacheHit = core.getState('cache-hit')
-    const key = core.getState('key')
+  throw new Error('🔥 This is a manual test error from v4.0.2')
+  // try {
+  //   core.warning('Starting cache save process...')
+  //   const cacheHit = core.getState('cache-hit')
+  //   const key = core.getState('key')
 
-    if (cacheHit === 'false') {
-      const cachePath = core.getState('cache-path')
-      const path = core.getState('path')
+  //   if (cacheHit === 'false') {
+  //     const cachePath = core.getState('cache-path')
+  //     const path = core.getState('path')
 
-      await exec(`mkdir -p ${cachePath}`)
-      const cp = await exec(`cp -r ./${path} ${cachePath}`)
+  //     await exec(`mkdir -p ${cachePath}`)
+  //     const cp = await exec(`cp -r ./${path} ${cachePath}`)
 
-      core.debug(cp.stdout)
-      if (cp.stderr) core.error(cp.stderr)
-      if (!cp.stderr) core.info(`Cache saved with key ${key}`)
-    } else {
-      core.info(`Cache hit on the key ${key}`)
-      core.info(`,not saving cache`)
-    }
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+  //     core.debug(cp.stdout)
+  //     if (cp.stderr) core.error(cp.stderr)
+  //     if (!cp.stderr) core.info(`Cache saved with key ${key}`)
+  //   } else {
+  //     core.info(`Cache hit on the key ${key}`)
+  //     core.info(`,not saving cache`)
+  //   }
+  // } catch (error) {
+  //   if (error instanceof Error) core.setFailed(error.message)
+  // }
 }
 
 run()
